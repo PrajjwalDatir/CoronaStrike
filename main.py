ccbb7b8ac579b5b32c54e3e250d252293b50f717
Prajjwal Datir <46681482+PrajjwalDatir@users.noreply.github.com>
2020-04-28 22:30:51 +0530

    working 0.0.1

--- main.py
+++ main.py
@@ -1,30 +1,18 @@
 import pygame
 import time
 import random
+import math
 
 
-#Colours:
-Black = (0, 0, 0)
-White = (255, 255, 255)
-Red = (255, 0, 0)
-Green = (0, 255, 0)
-Blue = (0, 0, 255)
-Yellow = (255,255,0)
-Aqua = (0, 255, 255)
-Water = (48, 86, 191)
-Pink = (255, 0, 255)
-Lawn_Green = (124, 252, 0)
-Sky_blue = (0,191,255)
-Sandy_Brown = (244, 164, 96)
 
 #to initialize the pygame module
 pygame.init()
 
-#background Image
-background = pygame.image.load('evening_1.jpg')
-
 # To start the screen at 600X800 resolution.
 screen = pygame.display.set_mode((800, 600))
+
+#background Image
+background = pygame.image.load('evening_1.jpg').convert()
 
 #Title and Icon
 pygame.display.set_caption("Corona Strike 0.0.1")
@@ -32,9 +20,20 @@ icon = pygame.image.load('Code_Maniac.jpg')
 pygame.display.set_icon(icon)
 
 running = True
+pygame.mixer.music.load('background.wav')
+pygame.mixer.music.play(-1)
 
-#To make it full screen - 
-# pygame.display.toggle_fullscreen()
+# Game Over
+over_font = pygame.font.Font('freesansbold.ttf', 64)
+
+# score functions
+score = 0
+font = pygame.font.Font('DoubleFeature20.ttf', 48)
+textX = 10
+textY = 10
+
+#To make 	it full screen - 
+#pygame.display.toggle_fullscreen()
 
 #Player - Protogonist
 playerImg = pygame.image.load('spaceship48X48.png')
@@ -44,39 +43,58 @@ coor_player = (playerX, playerY)
 player_movX = 0
 player_movY = 0
 
-
-def player(x, y):
-	coor_player = (x, y)	
-	screen.blit(playerImg, coor_player)
-
 #Bullet - kill la kill
 bulletImg = pygame.image.load('bullet_1.png')
 bulletX = 0
 bulletY = 0
-# coor_bullet = (bulletX, bulletY)
-bullet_movY = 0.1
-bullet_state = False
-
-def fire_bullet(x, y):
-	# global bullet_state # bullet is currently fired. and you can see it	
-	screen.blit(bulletImg, (x + 12, y + 5))
+bullet_movY = 5
+bullet_state = 'ready'
 
 #level 1 corona-virus
 corona_1_Img = pygame.image.load('corona_1.png')
-corona_1X = random.randint(0, 752)
+corona_1X = random.randint(100, 600)
 corona_1Y = random.randint(50, 150)
-corona_movX = 0.5
+corona_movX = 1
 corona_movY = 48
+
+def my_score():
+	textX = 10
+	textY = 10 
+	blit_score = font.render("SCORE : " + str(score), True, (255, 255, 255))
+	screen.blit(blit_score, (textX, textY))
+
+
+def game_over_text():
+    over_text = over_font.render("GAME OVER", True, (255, 255, 255))
+    screen.blit(over_text, (200, 250))
+
+
+def player(x, y):	
+	screen.blit(playerImg, (x, y))
+
+
+def fire_bullet(x, y):
+	global bullet_state # bullet is currently fired. and you can see it	
+	if bullet_state == 'fired':
+		screen.blit(bulletImg, (x + 10, y - 15))
+
 
 def corona_1(corona_1X, corona_1Y):	
 	screen.blit(corona_1_Img, (corona_1X, corona_1Y))
+
+
+def isCollision(enemyX, enemyY, bulletX, bulletY):
+	distance = math.hypot(enemyX - bulletX, enemyY - bulletY)
+	if distance < 27:
+		return True
+	return False
 
 
 #main game-loop
 while running:
 
 	# Background color
-	screen.fill(Water)
+	screen.fill((0,0,0))
 	# Background Image
 	screen.blit(background, (0, 0))
 	
@@ -84,7 +102,7 @@ while running:
 	player(playerX, playerY)
 	corona_1(corona_1X, corona_1Y)
 	#sleep(5)
-
+	my_score()
 
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
@@ -92,30 +110,33 @@ while running:
 	# if keystroke is pressed check its left or right
 		if event.type == pygame.KEYDOWN:
 			if event.key == pygame.K_RIGHT:
-				player_movX = 1
+				player_movX = 2
 				print("right")
 			elif event.key == pygame.K_LEFT:
-				player_movX = -1
+				player_movX = -2
 				print("left")
 			elif event.key == pygame.K_UP:
-				player_movY = -1
+				player_movY = -2
 				print("Up")
 			elif event.key == pygame.K_DOWN:
-				player_movY = 1
+				player_movY = 2
 				print("Down")
 			elif event.key == pygame.K_SPACE:
 	  			print("Shoot.")
-	  			bullet_state = True
-	  			bulletX = playerX
-	  			bulletY = playerY
-	  			fire_bullet(bulletX, bulletY)
+	  			if bullet_state == 'ready':
+	  				bullet_sound = pygame.mixer.Sound('laser.wav')
+	  				bullet_sound.play()
+	  				bullet_state = 'fired'
+	  				bulletX = playerX
+	  				bulletY = playerY
+	  				fire_bullet(bulletX, bulletY)
 			elif event.key == pygame.K_ESCAPE:
 	  			print("Escape key.")
 			else:
 				pass
 		if event.type == pygame.KEYUP:
 			if event.key == pygame.K_SPACE:
-	  			print("dhuddhud")
+				print("space released")
 			elif event.key == pygame.K_ESCAPE:
 				running = False
 				print("Existing the Game.")
@@ -131,8 +152,8 @@ while running:
 			elif event.key == pygame.K_DOWN:
 				player_movY = 0
 				print("Down")
-			else:
-				pass
+			
+
 	# Boundary conditions : don't leave the screen player
 
 		#for player
@@ -148,9 +169,25 @@ while running:
 	playerY += player_movY
 
 		#for Bullet
-	if bullet_state == True:
+	if bullet_state == 'fired':
 		fire_bullet(bulletX, bulletY)
 		bulletY -= bullet_movY
+	if bulletY <= 0:
+		bullet_state = 'ready'
+	
+		# collision
+	if isCollision(corona_1X, corona_1Y, bulletX, bulletY):
+		explosion_sound = pygame.mixer.Sound('explosion.wav')
+		explosion_sound.play()
+		bullet_state = 'ready'
+		bulletY = 800
+		score += 1
+		corona_1X = random.randint(100, 600)
+		corona_1Y = random.randint(50, 150)
+		print(score)
+
+	if isCollision(corona_1X, corona_1Y, playerX, playerY):
+		running = False
 
 		#for corona_1
 	if corona_1X <= 0:
@@ -166,4 +203,8 @@ while running:
 	elif corona_1Y >= 552:
 		corona_1Y = 552
 	corona_1X += corona_movX
+	if running == False:
+		game_over_text()
 	pygame.display.update()
+	
+time.sleep(0.5)
