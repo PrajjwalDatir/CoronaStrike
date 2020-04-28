b7797d35105800a820535b233e9d049b609690cd
Prajjwal Datir <46681482+PrajjwalDatir@users.noreply.github.com>
2020-04-29 01:51:40 +0530

    issue with spawning of enemies

--- learn.py
+++ learn.py
@@ -4,6 +4,21 @@ import random
 import math
 
 
+#Colours:
+Black = (0, 0, 0)
+White = (255, 255, 255)
+Red = (255, 0, 0)
+Green = (0, 255, 0)
+Blue = (0, 0, 255)
+Yellow = (255,255,0)
+Aqua = (0, 255, 255)
+Water = (48, 86, 191)
+Pink = (255, 0, 255)
+Lawn_Green = (124, 252, 0)
+Sky_blue = (0,191,255)
+Sandy_Brown = (244, 164, 96)
+
+My_colours = [Sandy_Brown, Sky_blue, Lawn_Green, Black, White, Red, Green, Blue, Yellow, Aqua, Water, Pink]
 
 #to initialize the pygame module
 pygame.init()
@@ -19,18 +34,19 @@ pygame.display.set_caption("Corona Strike 0.0.1")
 icon = pygame.image.load('Code_Maniac.jpg')
 pygame.display.set_icon(icon)
 
+# number of enemies in level 1
+
+
 running = True
-pygame.mixer.music.load('background.wav')
-pygame.mixer.music.play(-1)
+# pygame.mixer.music.load('background.wav')
+# pygame.mixer.music.play(-1)
 
 # Game Over
-over_font = pygame.font.Font('freesansbold.ttf', 64)
+over_font = pygame.font.Font('freesansbold.ttf', 96)
 
 # score functions
 score = 0
-font = pygame.font.Font('DoubleFeature20.ttf', 48)
-textX = 10
-textY = 10
+font = pygame.font.Font('DoubleFeature20.ttf', 24)
 
 #To make 	it full screen - 
 #pygame.display.toggle_fullscreen()
@@ -53,24 +69,45 @@ bullet_state = 'ready'
 bullet_vel = 1
 
 #level 1 corona-virus
-corona_1_Img = pygame.image.load('corona_1.png')
-corona_1X = random.randint(100, 600)
-corona_1Y = random.randint(50, 150)
-corona_movX = 1
-corona_movY = 48
-corona_vel = 0.3
+corona_1_Img = []
+corona_1X = []
+corona_1Y = []
+corona_choice = [30, 78, 126, 174]
+for i in range(5):
+	corona_1_Img.append(pygame.image.load('corona_1.png'))
+	again = True
+	while again:
+		corona_1X.append(random.randint(100, 600))
+		corona_1Y.append(random.choice(corona_choice))
+		if i == 0:
+			break
+		for j in range(0, i):
+			print("[", corona_1X, ",", corona_1Y)
+			distance = math.hypot(corona_1X[i] - corona_1X[j], corona_1Y[i] - corona_1Y[j])
+			if distance < 60:
+				again = True
+				corona_1X.pop()
+				corona_1Y.pop()
+				break
+			else:
+				again = False
+				break
 
+corona_movX = [1]*5
+corona_movY = [48]*5
+corona_vel = 0.3
 
 def my_score():
 	textX = 10
-	textY = 10 
+	textY = 10
 	blit_score = font.render("SCORE : " + str(score), True, (255, 255, 255))
 	screen.blit(blit_score, (textX, textY))
 
 
 def game_over_text():
+
     over_text = over_font.render("GAME OVER", True, (255, 255, 255))
-    screen.blit(over_text, (200, 250))
+    screen.blit(over_text, (100, 300))
 
 
 def player(x, y):	
@@ -84,34 +121,37 @@ def fire_bullet(x, y):
 
 
 def corona_1(corona_1X, corona_1Y):	
-	screen.blit(corona_1_Img, (corona_1X, corona_1Y))
+	for i in range(5):
+		screen.blit(corona_1_Img[i], (int(corona_1X[i]), int(corona_1Y[i])))
 
 
 def isCollision(enemyX, enemyY, bulletX, bulletY):
 	distance = math.hypot(enemyX - bulletX, enemyY - bulletY)
-	if distance < 27:
+	if distance < 30:
 		return True
 	return False
 
 # FPS
-pygame.display.set_caption("FPS")
+def my_fps(dt):
+	textX = 700
+	textY = 10 
+	blit_fps = font.render("FPS : " + str(dt), True, (255, 255, 255))
+	screen.blit(blit_fps, (textX, textY))
+
 	
 clock = pygame.time.Clock()
-
 #main game-loop
 while running:
-
 	dt = clock.tick(30)
 	# Background color
 	screen.fill((0,0,0))
 	# Background Image
 	screen.blit(background, (0, 0))
-	
-
 	player(playerX, playerY)
 	corona_1(corona_1X, corona_1Y)
 	#sleep(5)
 	my_score()
+	my_fps(dt)
 
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
@@ -177,34 +217,41 @@ while running:
 		bullet_state = 'ready'
 	
 		# collision
-	if isCollision(corona_1X, corona_1Y, bulletX, bulletY):
-		explosion_sound = pygame.mixer.Sound('explosion.wav')
-		explosion_sound.play()
-		bullet_state = 'ready'
-		bulletY = 800
-		score += 1
-		corona_1X = random.randint(100, 600)
-		corona_1Y = random.randint(50, 150)
-		print(score)
+	for i in range(5):
+		for j in range(i + 1, 5):
+			if isCollision(corona_1X[i], corona_1Y[i], corona_1X[j], corona_1Y[j]):		
+				corona_movX[i] = -corona_movX[i]
+				corona_movX[j] = -corona_movX[j]
 
-	if isCollision(corona_1X, corona_1Y, playerX, playerY):
-		running = False
+		if isCollision(corona_1X[i], corona_1Y[i], bulletX, bulletY):
+			explosion_sound = pygame.mixer.Sound('explosion.wav')
+			explosion_sound.play()
+			bullet_state = 'ready'
+			bulletY = 800
+			score += 1
+			corona_1X[i] = random.randint(100, 600)
+			corona_1Y[i] = random.choice(corona_choice)
+			print(score)
+
+		if isCollision(corona_1X[i], corona_1Y[i], playerX, playerY):
+			running = False
 
 		#for corona_1
-	if corona_1X <= 0:
-		corona_1X = 0
-		corona_movX = -corona_movX
-		corona_1Y += corona_movY	
-	elif corona_1X >= 752:
-		corona_1X = 752
-		corona_movX = -corona_movX
-		corona_1Y += corona_movY
-	if corona_1Y <= 0:
-		corona_1Y = 0		
-	elif corona_1Y >= 552:
-		corona_1Y = 552
-	corona_1X += corona_movX * (dt * corona_vel)
-	
+	for i in range(5):
+		if corona_1X[i] <= 0:
+			corona_1X[i] = 0
+			corona_movX[i] = -corona_movX[i]
+			corona_1Y[i] += corona_movY[i]	
+		elif corona_1X[i] >= 752:
+			corona_1X[i] = 752
+			corona_movX[i] = -corona_movX[i]
+			corona_1Y[i] += corona_movY[i]
+		if corona_1Y[i] <= 0:
+			corona_1Y[i] = 0		
+		elif corona_1Y[i] >= 552:
+			corona_1Y[i] = 552
+		corona_1X[i] += corona_movX[i] * (dt * corona_vel)
+		
 	if running == False:
 		game_over_text()
 	pygame.display.update()
